2eb1027b5c90b7a0dbec37e7bdb0af32b1d359d6
Arghya211994 <arghya@himanstech.com>
2025-09-19 15:59:57 +0530

    loader removed

--- src/main.jsx
+++ src/main.jsx
@@ -7,31 +7,11 @@ import { store } from './redux/store.js'
 import Loader from './components/Loader/Loader.jsx'
 
 
-const Root = () => {
-  const [loading, setLoading] = useState(true);
-
-  useEffect(() => {
-    // Simulate loading (you can replace with API call or store check)
-    const timer = setTimeout(() => {
-      setLoading(false);
-    }, 2000);
-
-    return () => clearTimeout(timer);
-  }, []);
-
-  if (loading) {
-    return <Loader />;
-  }
-
-  return (
-    <Provider store={store}>
-      <App />
-    </Provider>
-  );
-};
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <Root />
+    <Provider store={store}>
+      <App />
+    </Provider>
   </StrictMode>
 );
